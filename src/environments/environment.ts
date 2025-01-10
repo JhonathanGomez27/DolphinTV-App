f5a6e73f41d39e2b03efbd5cc640514e29f541cf
JhonathanGomez27 <jhonathango2741@gmail.com>
2025-01-10 14:39:14 -0500

    update environment configuration for development and production URLs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
-    urlAuth: 'http://127.0.0.1:3000/api/authentication/',
-    url: 'http://127.0.0.1:3000/api/',
-    urlImages: 'http://127.0.0.1',
-    // urlAuth: 'http://100.29.133.0:4000/api/authentication/',
-    // url: 'http://100.29.133.0:4000/api/',
-    // urlImages: 'http://100.29.133.0',
+    // urlAuth: 'http://127.0.0.1:4000/api/authentication/',
+    // url: 'http://127.0.0.1:4000/api/',
+    // urlImages: 'http://127.0.0.1',
+    urlAuth: 'http://100.29.133.0:4000/api/authentication/',
+    url: 'http://100.29.133.0:4000/api/',
+    urlImages: 'http://100.29.133.0',
     pagination: 20,
     production: false
 };
